07f6a060fb670af47c6fd5e940d7bf1b0ae14181
Vivek Madiyan <147875476+vivekmadiyan@users.noreply.github.com>
2026-01-23 14:48:25 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,9 @@
-iimport { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Render expects "build" as publish dir
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-      '/socket.io': {
-        target: 'http://localhost:5000',
-        ws: true,
-      }
-    }
+    outDir: "build",
   }
 });
