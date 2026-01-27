2786ccada3ae135dc5fffb7c3e5163ead063698a
Vivek Madiyan <147875476+vivekmadiyan@users.noreply.github.com>
2026-01-27 18:52:14 +0530

    Refactor Client component for improved Avatar display
    Updated Avatar component properties for better styling.

--- client/src/components/Client.jsx
+++ client/src/components/Client.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import Avatar from 'react-avatar';
+import React from "react";
+import Avatar from "react-avatar";
 
-const Client = ({username}) => {
-    return (
-        <div className="client">
-            <Avatar name={username} size={50} round="14px" />
-            <span className="userName">{username}</span>
-        </div>
-    );
+const Client = ({ username }) => {
+  return (
+    <div className="client">
+      <Avatar
+        name={username}
+        size="44"
+        round="12px"
+        textSizeRatio={2}
+      />
+      <span className="userName">{username}</span>
+    </div>
+  );
 };
 
 export default Client;
